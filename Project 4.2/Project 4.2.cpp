e59bb1e17dde031b9644e27c71d502136843c245
Orest Yakymiv <akimivorest95@gmail.com>
2020-11-02 16:03:15 +0200

    first commit

---
+++ Project 4.2/Project 4.2.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, A, B, y;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(7) << "y" << " |" << endl;
+	cout << "---------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		A = fabs(9*x*x*x+2);
+		if (x < 4)
+			B = 3 * pow(x,5)-pow(x,3) + 2*x - 1 ;
+		else
+			if (x>=7)
+				B = log10(pow(2*x,-1)+exp(3*x+1));
+			else
+				B = atan(x-2)/3;
+		y = A + B;
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << y
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+	return 0;
+}
